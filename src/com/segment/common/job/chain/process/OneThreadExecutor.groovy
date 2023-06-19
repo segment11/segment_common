b62f0dd49736ee842e8e20d50dcfd770fab26bb9
kerry <dingyong87@163.com>
2023-06-19 13:25:19 +0800

    1. change application job executor queue size  to 0.

--- src/com/segment/common/job/chain/process/OneThreadExecutor.groovy
+++ src/com/segment/common/job/chain/process/OneThreadExecutor.groovy
@@ -3,7 +3,6 @@ package com.segment.common.job.chain.process
 import com.segment.common.job.NamedThreadFactory
 import groovy.transform.CompileStatic
 
-import java.util.concurrent.LinkedBlockingDeque
 import java.util.concurrent.LinkedTransferQueue
 import java.util.concurrent.ThreadPoolExecutor
 import java.util.concurrent.TimeUnit
@@ -12,13 +11,13 @@ import java.util.concurrent.TimeUnit
 class OneThreadExecutor extends ThreadPoolExecutor {
     OneThreadExecutor(String threadName) {
         super(1, 1, 0, TimeUnit.MILLISECONDS,
-                new OneSizeExecutorQueue(), new NamedThreadFactory(threadName), new AbortPolicy())
+                new ZeroSizeExecutorQueue(), new NamedThreadFactory(threadName), new AbortPolicy())
     }
 }
 
 @CompileStatic
-class OneSizeExecutorQueue extends LinkedBlockingDeque<Runnable> {
+class ZeroSizeExecutorQueue extends LinkedTransferQueue<Runnable> {
     boolean offer(Runnable o) {
-        this.size() == 0 ? super.offer(o) : false
+        false
     }
 }
